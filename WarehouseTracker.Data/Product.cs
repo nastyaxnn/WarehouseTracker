f41b0bf32848966c3c8eb18b0cdab7298a620bc9
Sivolap A.V. <nastyaxnn@mail.ru>
2026-09-10 22:27:10 +0300

    Добавить модель и хранилище данных

---
+++ WarehouseTracker.Data/Product.cs
@@ -0,0 +1,8 @@
+namespace WarehouseTracker.Data;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public int Quantity { get; set; }
+}
---
+++ WarehouseTracker.Data/ProductRepository.cs
@@ -0,0 +1,18 @@
+namespace WarehouseTracker.Data;
+
+public class ProductRepository
+{
+    private readonly List<Product> _items = new()
+    {
+        new Product { Id = 1, Name = "Клавиатура",  Quantity = 15 },
+        new Product { Id = 2, Name = "Мышь",        Quantity = 4  }, // < 10
+        new Product { Id = 3, Name = "Монитор",     Quantity = 7  }, // < 10
+        new Product { Id = 4, Name = "Кабель USB",  Quantity = 100 },
+        new Product { Id = 5, Name = "Наушники",    Quantity = 3  }  // < 10
+    };
+
+    public List<Product> GetAll()
+    {
+        return _items;
+    }
+}
